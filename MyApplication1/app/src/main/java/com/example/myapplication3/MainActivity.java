b1fea369f70904597ac1b6193cc43fdba714934f
胡鹏 <64hp@163.com>
2019-08-18 19:17:47 +0800

    Hello world

---
+++ MyApplication1/app/src/main/java/com/example/myapplication3/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.myapplication3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d("MainActivity", "on_create-Bunlde-method");
+        Log.i(TAG, "onCreate: ---");
+    }
+
+}
