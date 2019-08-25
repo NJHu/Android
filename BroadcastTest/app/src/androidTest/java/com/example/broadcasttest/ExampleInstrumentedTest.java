750c8003bdbbd3a391069d4e075985fc870eb7cc
胡鹏 <64hp@163.com>
2019-08-25 23:09:06 +0800

    广播的静态和动态注册

--- BroadcastTest/app/src/androidTest/java/com/example/broadcasttest/ExampleInstrumentedTest.java
+++ BroadcastTest/app/src/androidTest/java/com/example/broadcasttest/ExampleInstrumentedTest.java
@@ -1,6 +1,7 @@
 package com.example.broadcasttest;
 
 import android.content.Context;
+
 import androidx.test.InstrumentationRegistry;
 import androidx.test.runner.AndroidJUnit4;
 
--- BroadcastTest/app/src/main/java/com/example/broadcasttest/MainActivity.java
+++ BroadcastTest/app/src/main/java/com/example/broadcasttest/MainActivity.java
@@ -1,13 +1,51 @@
 package com.example.broadcasttest;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
+import android.net.Network;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private IntentFilter intentFilter;
+    private NetworkChangeReceiver networkChangeReceiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        intentFilter = new IntentFilter("android.net.conn.CONNECTIVITY_CHANGE");
+        networkChangeReceiver = new NetworkChangeReceiver();
+        registerReceiver(networkChangeReceiver, intentFilter);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(networkChangeReceiver);
+    }
+
+    class NetworkChangeReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+//            Toast.makeText(context, "newwork change", Toast.LENGTH_SHORT).show();
+
+            ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+            Network network = connectivityManager.getActiveNetwork();
+            if (network != null) {
+//                Toast.makeText(context, "newwork change 1", Toast.LENGTH_SHORT).show();
+            }else  {
+//                Toast.makeText(context, "newwork change 0", Toast.LENGTH_SHORT).show();
+            }
+
+        }
+
     }
 }
