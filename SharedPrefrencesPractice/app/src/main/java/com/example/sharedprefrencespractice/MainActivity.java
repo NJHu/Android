225d92496b795da82270088bd00b18a152fabf04
胡鹏 <64hp@163.com>
2019-09-05 13:31:17 +0800

    偏好设置存储

---
+++ SharedPrefrencesPractice/app/src/main/java/com/example/sharedprefrencespractice/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.sharedprefrencespractice;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText editText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editText = findViewById(R.id.edit_text);
+        Button saveBtn = findViewById(R.id.save_btn);
+        Button readBtn = findViewById(R.id.read_btn);
+
+        saveBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                
+            }
+        });
+
+        readBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+    }
+}
