e05664c3941dbd131480279b48c406d43eba1d73
胡鹏 <64hp@163.com>
2019-09-06 21:44:01 +0800

    Update MainActivity.java

--- SharedPrefrencesPractice/app/src/main/java/com/example/sharedprefrencespractice/MainActivity.java
+++ SharedPrefrencesPractice/app/src/main/java/com/example/sharedprefrencespractice/MainActivity.java
@@ -23,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
         saveBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                
+
             }
         });
 
