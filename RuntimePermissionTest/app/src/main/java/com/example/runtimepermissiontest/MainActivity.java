36a13cee736808cb30eab9c9f30fbd84f3625b6b
胡鹏 <64hp@163.com>
2019-09-08 13:54:51 +0800

    权限申请

--- RuntimePermissionTest/app/src/main/java/com/example/runtimepermissiontest/MainActivity.java
+++ RuntimePermissionTest/app/src/main/java/com/example/runtimepermissiontest/MainActivity.java
@@ -1,8 +1,19 @@
 package com.example.runtimepermissiontest;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +21,38 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button callBtn = findViewById(R.id.call_btn);
+        callBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                boolean hasPermission = ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CALL_PHONE) == PackageManager.PERMISSION_GRANTED;
+                if (hasPermission) {
+                    MainActivity.this.call();
+                }else  {
+                    ActivityCompat.requestPermissions
+                            (MainActivity.this, new String[]{Manifest.permission.CALL_PHONE}, 1);
+                }
+            }
+        });
+    }
+    private void call() {
+        try {
+            Intent intent = new Intent(Intent.ACTION_CALL);
+            intent.setData(Uri.parse("tel:10086"));
+            MainActivity.this.startActivity(intent);
+        } catch (SecurityException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == 1) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                call();
+            }else  {
+                Toast.makeText(MainActivity.this, "error", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
