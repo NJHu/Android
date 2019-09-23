1a8d5c6a2e19daee4c3d5c1c1d65bf70afbcd158
胡鹏 <64hp@163.com>
2019-09-23 21:13:32 +0800

    主线程UI和线程切换

---
+++ AndroidThreadTest/app/src/main/java/com/example/androidthreadtest/MainActivity.java
@@ -0,0 +1,75 @@
+package com.example.androidthreadtest;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.ProcessingInstruction;
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+    TextView textView = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.content_text_view);
+        Button button = findViewById(R.id.change_text);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MainActivity.this.changeText2();
+            }
+        });
+    }
+
+    public static final int UPDATE_TEXT = 1;
+    private Handler handler = new Handler() {
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            super.handleMessage(msg);
+            switch (msg.what) {
+                case UPDATE_TEXT:
+                    textView.setText("wo shi xin text");
+                    break;
+                default:
+                    break;
+            }
+        }
+    };
+
+    private void changeText2() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Message message = new Message();
+                message.what = UPDATE_TEXT;
+                handler.handleMessage(message);
+            }
+        }).start();
+    }
+
+    private void changeText() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Log.d(TAG, "run: " + Thread.currentThread().toString());
+                        MainActivity.this.textView.setText("nice to meet you");
+                    }
+                });
+            }
+        }).start();
+    }
+}
