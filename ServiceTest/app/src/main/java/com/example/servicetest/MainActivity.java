c949f363d39a8c449dbceaac0e5c464ed51960d1
胡鹏 <64hp@163.com>
2019-09-24 21:40:55 +0800

    Service初探

--- ServiceTest/app/src/main/java/com/example/servicetest/MainActivity.java
+++ ServiceTest/app/src/main/java/com/example/servicetest/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.servicetest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -20,5 +21,27 @@ public class MainActivity extends AppCompatActivity {
                 new DownloadTask().execute();
             }
         });
+
+
+        Button startbutton = findViewById(R.id.start_service);
+        startbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent startIntent = new Intent(MainActivity.this, MyService.class);
+                MainActivity.this.startService(startIntent);
+
+            }
+        });
+
+        Button stopbutton = findViewById(R.id.stop_service);
+        stopbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent stopIntent = new Intent(MainActivity.this, MyService.class);
+                MainActivity.this.stopService(stopIntent);
+
+            }
+        });
+
     }
 }
