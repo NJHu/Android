b39d18884f5432cb295af474977999a8dd49cad9
胡鹏 <64hp@163.com>
2019-10-09 16:46:08 +0800

    活动和服务交互

--- ServiceTestB/app/src/main/java/com/example/servicetestb/MainActivity.java
+++ ServiceTestB/app/src/main/java/com/example/servicetestb/MainActivity.java
@@ -2,25 +2,48 @@ package com.example.servicetestb;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ComponentName;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MainActivity";
+
+    private MyService.DownloadBinder downloadBinder;
+
+    private ServiceConnection connection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
+            Log.d(TAG, "onServiceConnected: ");
+            downloadBinder = (MyService.DownloadBinder) iBinder;
+            downloadBinder.startDownload();
+            downloadBinder.getProgress();
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName componentName) {
+            Log.d(TAG, "onServiceDisconnected: ");
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ((Button)findViewById(R.id.start)).setOnClickListener(new View.OnClickListener() {
+        ((Button) findViewById(R.id.start)).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 MainActivity.this.start();
             }
         });
 
-        ((Button)findViewById(R.id.stop)).setOnClickListener(new View.OnClickListener() {
+        ((Button) findViewById(R.id.stop)).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 MainActivity.this.stop();
@@ -29,11 +52,16 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void start() {
-        Intent intent = new Intent(this, MyService.class);
-        startService(intent);
+//        Intent intent = new Intent(this, MyService.class);
+//        startService(intent);
+        Intent bindIntent = new Intent(this, MyService.class);
+        bindService(bindIntent, connection, BIND_AUTO_CREATE);
+
     }
+
     private void stop() {
-        Intent intent = new Intent(this, MyService.class);
-        stopService(intent);
+//        Intent intent = new Intent(this, MyService.class);
+//        stopService(intent);
+        unbindService(connection);
     }
 }
