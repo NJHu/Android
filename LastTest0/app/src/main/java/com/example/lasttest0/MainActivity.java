cc79cf4ec1496e6b33271761dde2ccc0e2262195
胡鹏 <64hp@163.com>
2019-11-03 13:53:10 +0800

    test

---
+++ LastTest0/app/src/main/java/com/example/lasttest0/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.lasttest0;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
